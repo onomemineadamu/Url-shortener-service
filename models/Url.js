e91330b93dc7511096c3d82210118d5b565cc71a
Onome Mine Adamu <onomemineadamu@gmail.com>
2019-08-30 18:37:48 +0200

    First Commit

---
+++ models/Url.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UrlSchema = new mongoose.Schema({
+  urlCode: String,
+  longUrl: String,
+  shortUrl: String,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Url', UrlSchema);
